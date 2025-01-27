35be0114552b66767cce272d778a030401c42a16
Hlaing Htun <hlainghtun2018@gmail.com>
2025-01-27 19:38:13 +0630

    update course

--- src/main/java/sspd/sms/courseoptions/services/Services.java
+++ src/main/java/sspd/sms/courseoptions/services/Services.java
@@ -1,104 +1,71 @@
 package sspd.sms.courseoptions.services;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
-import sspd.sms.config.AppConfig;
+
+import org.springframework.stereotype.Service;
 import sspd.sms.config.SpringContextHelper;
 import sspd.sms.courseoptions.db.Courseimpl;
 import sspd.sms.courseoptions.model.Course;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class Services {
 
-    private ApplicationContext context = SpringContextHelper.getContext();
+//    private ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+//
+//    private Courseimpl cdb = context.getBean(Courseimpl.class);
 
-    private Courseimpl cdb = context.getBean(Courseimpl.class);
+
+    ApplicationContext context = SpringContextHelper.getContext();
+
+    Courseimpl cdb= context.getBean(Courseimpl.class);
+
+    private List<Course> courseList = new ArrayList<>();
+
+
 
     public List<Course> getAllCourses() {
-
-            return cdb.getAllTask();
+        courseList.clear();
+        courseList.addAll(cdb.getAllTask());
+        return courseList;
     }
 
     public void insertCourse(Course course) {
-
-        int oldSize = getAllCourses().size();
-
-        cdb.insertTask(course);
-
-        int newSize = getAllCourses().size();
-
-        if(oldSize != newSize) {
-
-            showInformationDialog("Data Insert Successful","Course","Course Data Insert Successfully!!!");
-
-        }
-
-
-
+        handleCourseChange(() -> cdb.insertTask(course), "Course Data Insert Successfully!!!");
     }
 
     public void updateCourse(Course course) {
-
-           cdb.updateTask(course);
+        handleCourseChange(() -> cdb.updateTask(course), "Course Data Updated Successfully!!!");
     }
 
     public void deleteCourse(Course course) {
-
-        int oldSize = getAllCourses().size();
-
-        cdb.deleteTask(course);
-
-        int newSize = getAllCourses().size();
-
-        if(oldSize != newSize) {
-
-            showInformationDialog("Data Delete Successful","Course","Teacher Data Delete Successfully!!!");
-
-        }
-
-
-
-
+        handleCourseChange(() -> cdb.deleteTask(course), "Course Data Deleted Successfully!!!");
     }
 
-    public void importCourse(List<Course>list) {
-
-
-        int oldSize = getAllCourses().size();
-
-        cdb.bactchTask(list);
-
-        int newSize = getAllCourses().size();
-
-        if(oldSize != newSize) {
-
-            showInformationDialog("Data Import Successful","Course","Teacher Import Successfully!!!");
-
-        }
-
-
-
+    public void importCourse(List<Course> list) {
+        handleCourseChange(() -> cdb.bactchTask(list), "Courses Imported Successfully!!!");
     }
 
-    private void showErrorDialog(String title, String header, String content) {
-        Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle(title);
-            alert.setHeaderText(header);
-            alert.setContentText(content);
-            alert.showAndWait();
-        });
+    private void handleCourseChange(Runnable action, String successMessage) {
+        int oldSize = getAllCourses().size();
+        action.run();
+        int newSize = getAllCourses().size();
+        if (oldSize != newSize) {
+            showInformationDialog("Operation Successful", "Course", successMessage);
+        }
     }
 
     private void showInformationDialog(String title, String header, String content) {
-        javafx.application.Platform.runLater(() -> {
-            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(Alert.AlertType.INFORMATION);
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle(title);
             alert.setHeaderText(header);
             alert.setContentText(content);
@@ -107,30 +74,21 @@ public class Services {
     }
 
     public int getCoursesCount() {
-
-        return getAllCourses().size();
+        return courseList.size();
     }
 
     public int findCourseByName(String name) {
-
         return getAllCourses().stream()
                 .filter(course -> course.getCourse_name().equals(name))
                 .map(Course::getCourse_id)
                 .findFirst()
                 .orElse(-1);
-
-
     }
 
-    public List<String>getCourseNames() {
-
-       return getAllCourses().stream()
-                .map(Course::getCourse_name).sorted().collect(Collectors.toList());
-
-
+    public List<String> getCourseNames() {
+        return getAllCourses().stream()
+                .map(Course::getCourse_name)
+                .sorted()
+                .collect(Collectors.toList());
     }
-
-
-
-
 }
